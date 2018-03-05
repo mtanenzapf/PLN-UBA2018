6aa567aa0b56dec22b9065f5cbabb944329c90f5
mtanenzapf <mtanenzapf@gmail.com>
2018-03-05 18:54:01 -0300

    GeneralTASS stats

--- sentiment/stats.py
+++ sentiment/stats.py
@@ -1,16 +1,23 @@
 from sentiment.tass import InterTASSReader
+from tass import GeneralTASSReader
 from collections import defaultdict
 
-reader = InterTASSReader('TASS/InterTASS/tw_faces4tassTrain1000rc.xml')
-tweets = list(reader.tweets())  # iterador sobre los tweets
-x = list(reader.X())  # iterador sobre los contenidos de los tweets
-y = list(reader.y())  # iterador sobre las polaridades de los tweets
 
-print(len(tweets))
+def getStatistics(reader, corpus):
+    y = list(reader.y())  # iterador sobre las polaridades de los tweets
 
-polarity_tweets = defaultdict(int)
-	
-for polarity in y:
-	polarity_tweets[polarity] += 1
+    polarity_tweets = defaultdict(int)
 
-print(polarity_tweets)
+    for polarity in y:
+        polarity_tweets[polarity] += 1
+
+    print(corpus)
+    print("Cantidad total de tweets: " + str(len(y)))
+    print("Cantidad de tweets por cada valor de polaridad:")
+
+    for polarity in polarity_tweets.keys():
+    	print(polarity + ": " + str(polarity_tweets[polarity]))
+
+
+getStatistics(InterTASSReader('TASS/InterTASS/tw_faces4tassTrain1000rc.xml'), "InterTASS")
+getStatistics(GeneralTASSReader('TASS/GeneralTASS/general-tweets-train-tagged.xml'), "GeneralTASS")
